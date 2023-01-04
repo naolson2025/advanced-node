9da139125bc7b0e8e8741712a1e40d6ae950e312
Nick Olson <naolson.1819@gmail.com>
2023-01-03 23:13:24 -0600

    connected to mongo atlas

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config()
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
