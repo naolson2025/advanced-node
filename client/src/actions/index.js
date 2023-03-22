fb2d52373e8af1789bdb3f2e80ee7da994676a0a
Nick Olson <naolson.1819@gmail.com>
2023-03-22 16:50:28 -0500

    display image from S3

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import { FETCH_USER, FETCH_BLOGS, FETCH_BLOG } from './types';
 
-export const fetchUser = () => async dispatch => {
+export const fetchUser = () => async (dispatch) => {
   const res = await axios.get('/api/current_user');
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const handleToken = token => async dispatch => {
+export const handleToken = (token) => async (dispatch) => {
   const res = await axios.post('/api/stripe', token);
 
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const submitBlog = (values, file, history) => async dispatch => {
+export const submitBlog = (values, file, history) => async (dispatch) => {
   // retrieve the s3 presigned url from the server
   const uploadConfig = await axios.get('/api/upload');
 
@@ -26,19 +26,23 @@ export const submitBlog = (values, file, history) => async dispatch => {
     },
   });
 
-  const res = await axios.post('/api/blogs', values);
+  const res = await axios.post('/api/blogs', {
+    ...values,
+    // pass the S3 key to our server and save it in the database
+    imageUrl: uploadConfig.data.key,
+  });
 
   history.push('/blogs');
   dispatch({ type: FETCH_BLOG, payload: res.data });
 };
 
-export const fetchBlogs = () => async dispatch => {
+export const fetchBlogs = () => async (dispatch) => {
   const res = await axios.get('/api/blogs');
 
   dispatch({ type: FETCH_BLOGS, payload: res.data });
 };
 
-export const fetchBlog = id => async dispatch => {
+export const fetchBlog = (id) => async (dispatch) => {
   const res = await axios.get(`/api/blogs/${id}`);
 
   dispatch({ type: FETCH_BLOG, payload: res.data });
--- models/Blog.js
+++ models/Blog.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const blogSchema = new Schema({
+  imageUrl: String,
   title: String,
   content: String,
   createdAt: { type: Date, default: Date.now },
